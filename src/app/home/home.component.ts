43d90831596f293aab90d9ed77924b65aeb8474c
John Young <jayoung@asicentral.com>
2023-12-29 17:48:43 +0000

    Implement the new housinglocation interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,16 @@ import {HousingLocationComponent} from '../housing-location/housing-location.com
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
-}
+  readonly baseUrl = 'https://angular.dev/assets/tutorials/common';
+  
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+};
